86c9d4ad9ceee2d2812850f4066fde39ceb5889c
Mehdi030 <DEINE_EMAIL>
2025-03-15 21:47:41 +0100

    Backend und Frontend verbunden

--- backend/src/main/java/ecommerce/controller/ProductController.java
+++ backend/src/main/java/ecommerce/controller/ProductController.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.math.BigDecimal;
 import java.util.List;
 import java.util.UUID;
 
